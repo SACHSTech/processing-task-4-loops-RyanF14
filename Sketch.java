ee8027884f55d96a448f705fd054ba1e92f26a30
RyanF14 <ryan.fellin25@ycdsbk12.ca>
2024-03-20 14:29:19 -0400

    1/4 completed

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -16,21 +16,25 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    //background(255);
+    stroke(0);
+    strokeWeight(2);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    line(((width/2)),0,((width/2)),height);
+    line(0, height/2, width, height/2 );
+	  for(int lineX = 0; lineX <= width/2; lineX += (width/20)) {
+      line(lineX,0, lineX, height/2);
+    }
+    for(int lineY = 0; lineY <= width/2; lineY += (width/20)) {
+      line(0,lineY,height/2,lineY);
+    }
+    for (int circleX = (width/2)+((height/2)*8); circleX <= width; circleX += 30){
+      ellipse((circleX), height/2, 15, 15);
+    }
   }
-  
-  // define other methods down here.
 }
